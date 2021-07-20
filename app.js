b9807e5eddf5d72218d84768c96f96919cd697cc
nambatsu <53079276+nambatsu@users.noreply.github.com>
2021-07-21 02:24:40 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,17 @@
+$(function () {
+  var w = $(window).width()
+  var h = $(window).height()
+ 
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/iframe_api";
+  var firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  var player;
+  window.onYouTubeIframeAPIReady = function() {
+    player = new YT.Player('player', {
+      width: w,
+      height: h,
+      videoId: 'AYcQIdTi55I',
+    });
+  }
+});
